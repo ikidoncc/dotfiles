cc97831684c8e23b9e12dbb44b794ce3fad56daa
ikidon <ikidon.cc@gmail.com>
2025-02-16 17:53:26 -0300

    zsh-syntax-highlighting path fix

--- zsh/script.sh
+++ zsh/script.sh
@@ -6,7 +6,7 @@ zsh-syntax-highlighting() {
 	echo "INFO: Installing zsh-syntax-highlight!"
 	mkdir -p "${HOME}/.zsh"
 	rm -rf "${home}/.zsh/zsh-syntax-highlighting"
-	git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "${HOME}/zsh-syntax-highlighting"
+	git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "${HOME}/.zsh/zsh-syntax-highlighting"
 	echo "INFO: zsh-syntax-highlighting installed!"
 }
 
